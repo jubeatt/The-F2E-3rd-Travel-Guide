f2ab94056f62bbc0c152296fa0e987b1edc1c74e
jim <jimdevelopesite@gmail.com>
2021-11-15 04:37:04 +0800

    更新Title文字及favicon

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 // 設定首頁路徑
 module.exports = {
+  // publicPath 路徑設定
   publicPath: process.env.NODE_ENV === 'production' ? '/The-F2E-3rd-Travel-Guide/' : '/',
+  // css loader設定
   css: {
     loaderOptions: {
       sass: {
@@ -12,6 +14,25 @@ module.exports = {
            @import "@/assets/scss/global.scss";
         `
       }
+    }
+  },
+  // HTML模板標題設定
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = '台灣旅遊導覽平台'
+        return args
+      })
+  },
+  // 圖標設定
+  pwa: {
+    iconPath: {
+      favicon32: 'favicon.jpg',
+      favicon16: 'favicon.jpg',
+      appleTouchIcon: 'favicon.jpg',
+      maskIcon: 'favicon.jpg',
+      msTileImage: 'favicon.jpg'
     }
   }
 }
